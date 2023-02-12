1890e0e52efae993246b61310da34befb90cf42a
user <user@DESKTOP-2VC9EF1>
2023-02-12 13:17:00 +0530

    Addition program

---
+++ src/test/java/tests/Test1.java
@@ -0,0 +1,15 @@
+package tests;
+
+public class Test1 
+{
+
+	public static void main(String[] args) 
+	{
+		int a=10;
+		int b=30;
+		int c=a+b;
+		System.out.println("addition value is:"+c);
+
+	}
+
+}
